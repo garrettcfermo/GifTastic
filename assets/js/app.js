f5190a651adbb13fe5f7f38ad53ac535509ac7c6
Garrett Fermo <garrettcfermo@gmail.com>
2018-09-04 13:20:07 -0700

    API Setup

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+// API 
+
+let url = "http://api.giphy.com/v1/gifs/search";
+url += '?' + $.param({
+  'api_key': "EVin2ZZKd1lWiJvD1LG1L0PoysDpYJKy",
+  'q': `surfing`,
+  'limit': 10
+});
+
+
+$.get(url)
+  .then(function(r) {
+    console.log(r.data)
+    r.data.forEach(gif => {
+      console.log(gif.images.original.url)
+      console.log(gif.rating)
+    });
+  })
+  .catch(function(e){console.log(e)})
+
+

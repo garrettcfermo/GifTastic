cafafb762eb96e3a5ae78ba38471eaee9f6d738a
Garrett Fermo <garrettcfermo@gmail.com>
2018-09-04 18:45:29 -0700

    Pull Gify's onto Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,45 @@
-// API Information
+//Create Sample Gif Buttons (Initial Load)
+sampleGif = ['surfing', 'wsl', 'waves', 'beaches', 'beer']
 
-let url = "http://api.giphy.com/v1/gifs/search";
-url += '?' + $.param({
-  'api_key': "EVin2ZZKd1lWiJvD1LG1L0PoysDpYJKy",
-  'q': `surfing`,
-  'limit': 10
+sampleGif.forEach(gif => {
+  $('#buttonArea').append(`
+     <button class="gifBtn" data-gif="${gif}"> ${gif}</button>
+   `)
 });
 
 
-$.get(url)
-  .then(function(r) {
-    console.log(r.data)
-    r.data.forEach(gif => {
-      console.log(gif.images.original.url)
-      console.log(gif.rating)
-    });
+//Adding new Gif Buttons
+$('#submitGif').on('click', function () {
+  event.preventDefault()
+  $('#buttonArea').append(`
+     <button class="gifBtn" data-gif="${$('#gifName').val().trim()}"> ${$('#gifName').val().trim()}</button>
+   `)
+  $('#gifName').val('')
+})
+
+
+// Pull the Gif Images and Display
+$(document).on('click', '.gifBtn', function () {
+
+  $('#gifArea').empty()
+
+  let url = "http://api.giphy.com/v1/gifs/search";
+  url += '?' + $.param({
+    'api_key': "EVin2ZZKd1lWiJvD1LG1L0PoysDpYJKy",
+    'q':`${$(this).attr('data-gif')}`,
+    'limit': 10
+  });
+
+
+  $.get(url)
+    .then(function (r) {
+      r.data.forEach(gif => {
+
+        $('#gifArea').append(`
+        <h4>${gif.rating}</h4>
+        <img src="${gif.images.original.url}" alt="">
+        `)
+      });
+    })
+    .catch(function (e) { console.log(e) })
   })
-  .catch(function(e){console.log(e)})
-
-
-// Sample Gif Data
-
-sampleGif=['surfing','wsl','waves','beaches','beer']

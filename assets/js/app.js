46caab80f06f8e24ed1ca24f4a38089749853e14
Garrett Fermo <garrettcfermo@gmail.com>
2018-09-04 19:39:44 -0700

    HTML Formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 //Create Sample Gif Buttons (Initial Load)
-sampleGif = ['surfing', 'wsl', 'waves', 'beaches', 'beer']
+sampleGif = ['surfing', 'wsl', 'waves', 'beach', 'beer']
 
 sampleGif.forEach(gif => {
   $('#buttonArea').append(`
@@ -9,7 +9,7 @@ sampleGif.forEach(gif => {
 
 
 //Adding new Gif Buttons
-$('#submitGif').on('click', function () {
+$('.submitGif').on('click', function () {
   event.preventDefault()
   $('#buttonArea').append(`
      <button class="gifBtn" data-gif="${$('#gifName').val().trim()}"> ${$('#gifName').val().trim()}</button>
@@ -27,17 +27,17 @@ $(document).on('click', '.gifBtn', function () {
   url += '?' + $.param({
     'api_key': "EVin2ZZKd1lWiJvD1LG1L0PoysDpYJKy",
     'q':`${$(this).attr('data-gif')}`,
-    'limit': 10
+    'limit': 9
   });
-
 
   $.get(url)
     .then(function (r) {
       r.data.forEach(gif => {
-
         $('#gifArea').append(`
-        <h4>${gif.rating}</h4>
-        <img src="${gif.images.original.url}" alt="">
+        <div class="col-md-4" >
+        <h4 class="gif-rating">${gif.rating}</h4>
+        <img class="gif-image" src="${gif.images.original.url}" alt=${$(this).attr('data-gif')}>
+        </div>
         `)
       });
     })
